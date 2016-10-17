7012b36c03fbf1716a29f33686ba8dd92fa1f43f
tshar <tanav.k20@gmail.com>
2016-10-17 19:23:22 -0400

    Week 7

---
+++ 10-17-2016/FIMP/src/main/java/inc/fimp/loginregisterfinal/LoginActivity.java
@@ -0,0 +1,110 @@
+/*************************************
+ * Team Name: The Team A
+ * Group Names: Alay Lad, Tanav Sharma, Hennok Tadesse
+ */
+
+package inc.fimp.loginregisterfinal;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonSignIn;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignup;
+
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser()!= null){
+            //profile act here
+            finish();
+            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+        }
+
+        progressDialog = new ProgressDialog(this);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        buttonSignIn = (Button) findViewById(R.id.buttonSignin);
+        textViewSignup = (TextView) findViewById(R.id.textViewSignUp);
+
+        buttonSignIn.setOnClickListener(this);
+        textViewSignup.setOnClickListener(this);
+    }
+
+    private void userLogin(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            //email is empty
+            Toast.makeText(LoginActivity.this, "Please enter an email", Toast.LENGTH_SHORT).show();
+            //stoping the fuction
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            //password is empty
+            Toast.makeText(LoginActivity.this, "Please enter a password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //if validations are ok
+        //we will first show a progress bar to the user
+
+        progressDialog.setMessage("Logging in..");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressDialog.dismiss();
+
+                            if(task.isSuccessful()){
+                                //starts user profile activity
+                                finish();
+                                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                            }
+                        }
+                    });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonSignIn){
+            userLogin();
+        }
+
+        if(view == textViewSignup){
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+
+    }
+}
---
+++ 10-17-2016/FIMP/src/main/java/inc/fimp/loginregisterfinal/UserInformation.java
@@ -0,0 +1,22 @@
+/*************************************
+ * Team Name: The Team A
+ * Group Names: Alay Lad, Tanav Sharma, Hennok Tadesse
+ */
+
+package inc.fimp.loginregisterfinal;
+
+/**
+ * Created by Tanav on 10/17/2016.
+ */
+public class UserInformation {
+
+    String name;
+
+    public UserInformation(){
+
+    }
+
+    public UserInformation(String name) {
+        this.name = name;
+    }
+}
