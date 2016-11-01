f3a6791ced3a41a228e76dbba32a727359c96e88
tshar <tanav.k20@gmail.com>
2016-10-31 21:50:51 -0400

    Update v3
    Created a menu_bar or toolbar. A basic toolbar for now, will be working
    on making it more pretty and attractive. Also may add or remove features
    to enhance user experience.
    
    I also updated my string file, to store the values.
    
    **Please take note that, the toasts being displayed is just a test, this
    will not be implemented in the code for obvious reasons.**

--- fimp/src/main/java/inc/fimp/MainActivity.java
+++ fimp/src/main/java/inc/fimp/MainActivity.java
@@ -2,6 +2,10 @@ package inc.fimp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        
+
+
+        return true;
+    }
+    
+    public boolean onOptionsItemSelected(MenuItem item){
+        int res_id = item.getItemId();
+        if(res_id==R.id.action_contact)
+        {
+            Toast.makeText(getApplicationContext(), "You selected Contacted us option", Toast.LENGTH_SHORT).show();
+        }
+
+        if(res_id==R.id.action_settings){
+            Toast.makeText(getApplicationContext(), "You selected Settings Option", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
